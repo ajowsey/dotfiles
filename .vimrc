07f583e9e3da88f4f7a91ce095eabb11c2d6f887
ajowsey <ajowsey@ualberta.ca>
2018-07-08 20:04:38 -0600

    Main files Added | Vim Color Added

---
+++ .vimrc
@@ -0,0 +1,8 @@
+syntax on
+colorscheme monokai
+
+set number
+set incsearch
+set hlsearch
+
+set laststatus=2
---
+++ install_script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf /home/pi/dotfiles/.vim /home/pi/.vim
+ln -sf /home/pi/dotfiles/.bashrc /home/pi/.bashrc
+ln -sf /home/pi/dotfiles/.vimrc /home/pi/.vimrc
+ln -sf /home/pi/dotfiles/.gitconfig /home/pi/.gitconfig
